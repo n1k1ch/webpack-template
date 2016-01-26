e295d1c3994cce1684c7e6c0b0a4a35c72fc4e7c
Nikita Cherevkov <ncherevkov@csod.com>
2016-01-26 19:30:09 +0200

    use 'postcss-loader' instead of deprecated 'autoprefixer-loader'

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ const path = require('path');
 const PATHS = {
     app: [path.join(__dirname, 'source/js/entry'), 'file?name=index.html!jade-html!' + path.join(__dirname, 'source/index.jade')],
     build: path.join(__dirname, 'dist')
-};
+},
+    autoprefixer = require('autoprefixer'),
+    precss       = require('precss');
 
 module.exports = {
     entry: PATHS.app,
@@ -13,11 +15,14 @@ module.exports = {
         path: PATHS.build,
         filename: 'bundle.js'
     },
+    postcss: function(){
+        return [autoprefixer, precss];
+    },
     module: {
         loaders: [
             {
                 test: /\.scss$/,
-                loader: "style!css!autoprefixer-loader!sass"
+                loader: "style!css!postcss-loader!sass"
             }
         ]
     }
